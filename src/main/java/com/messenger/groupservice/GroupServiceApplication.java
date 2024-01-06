43971e1917ee9425b989932d16d17e96e3b99634
denpool <den.dyrbalov25@gmail.com>
2024-01-06 19:02:34 +0100

    test

--- src/main/java/com/messenger/groupservice/GroupServiceApplication.java
+++ src/main/java/com/messenger/groupservice/GroupServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 public class GroupServiceApplication {
+    //test
     public static void main(String[] args) {
         SpringApplication.run(GroupServiceApplication.class, args);
     }
